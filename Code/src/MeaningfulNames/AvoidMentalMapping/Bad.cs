a1a0b3e6bd85167c93702b235ab241f309d51c8c
Mansour Tarafdar <tarafdar.mansour@gmail.com>
2022-08-30 06:46:42 +0400

    Avoid Mental Mapping

---
+++ Code/src/MeaningfulNames/AvoidMentalMapping/Bad.cs
@@ -0,0 +1,27 @@
+﻿namespace AvoidMentalMapping;
+
+public class Product
+{
+    public int Id { get; set; }
+    public int FooProperty { get; set; }
+}
+
+public class Bad
+{
+    public int CalculateProductSomeData(List<Product> products)
+    {
+        var result = 0;
+        foreach (var p in products)
+        {
+            // lots of code here
+
+            var evaluatedFooProperty = p.FooProperty;
+
+            result += evaluatedFooProperty;
+
+            // lots of code here
+        }
+
+        return result;
+    }
+}
---
+++ Code/src/MeaningfulNames/AvoidMentalMapping/Good.cs
@@ -0,0 +1,21 @@
+﻿namespace AvoidMentalMapping;
+
+public class Good
+{
+    public int CalculateProductSomeData(List<Product> products)
+    {
+        var result = 0;
+        foreach (var product in products)
+        {
+            // lots of code here
+
+            var evaluatedFooProperty = product.FooProperty;
+
+            result += evaluatedFooProperty;
+
+            // lots of code here
+        }
+
+        return result;
+    }
+}
