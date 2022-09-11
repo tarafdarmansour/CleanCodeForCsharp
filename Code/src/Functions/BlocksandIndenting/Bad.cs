c62795071d2204adfc8a505dad7480b4cecdd647
Mansour Tarafdar <tarafdar.mansour@gmail.com>
2022-09-11 07:24:25 +0400

    Fixing bug in BlocksandIndenting Bad code

--- Code/src/Functions/BlocksandIndenting/Bad.cs
+++ Code/src/Functions/BlocksandIndenting/Bad.cs
@@ -1,9 +1,11 @@
 ﻿namespace BlocksandIndenting;
-
+public class Bad
+{
 public Student Find(List<Student> list, int id)
 {
 Student r = null;foreach (var i in list)
 {
 if (i.Id == id)
     r = i;          }          return r;
+}
 }
