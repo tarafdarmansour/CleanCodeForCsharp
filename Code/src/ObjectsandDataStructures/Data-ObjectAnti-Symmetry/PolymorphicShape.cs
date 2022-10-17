8c4f51db9320de9ec31c6d708edf9806b5d77a34
Mansour Tarafdar <tarafdar.mansour@gmail.com>
2022-10-17 07:01:08 +0330

    feat (Chapter 6): add PolymorphicShape cs file

---
+++ Code/src/ObjectsandDataStructures/Data-ObjectAnti-Symmetry/PolymorphicShape.cs
@@ -0,0 +1,42 @@
+﻿using System.Drawing;
+
+namespace Data_ObjectAnti_Symmetry.PolymorphicShape;
+
+public class Shape
+{
+}
+
+public class Square : Shape
+{
+    private double side;
+    private Point topLeft;
+
+    public double area()
+    {
+        return side * side;
+    }
+}
+
+public class Rectangle : Shape
+{
+    private double height;
+    private Point topLeft;
+    private double width;
+
+    public double area()
+    {
+        return height * width;
+    }
+}
+
+public class Circle : Shape
+{
+    public const double PI = 3.141592653589793;
+    private Point center;
+    private double radius;
+
+    public double area()
+    {
+        return PI * radius * radius;
+    }
+}
