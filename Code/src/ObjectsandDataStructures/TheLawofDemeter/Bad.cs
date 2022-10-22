76dfda3af4146a2a3bf455b2d06a716f6a712087
Mansour Tarafdar <tarafdar.mansour@gmail.com>
2022-10-22 07:32:47 +0330

    feat (ObjectsandDataStructures): adding TheLawofDemeter source code example

---
+++ Code/src/ObjectsandDataStructures/TheLawofDemeter/Bad.cs
@@ -0,0 +1,35 @@
+﻿namespace TheLawofDemeter.Bad;
+
+public class Wallet
+{
+    public float Value { get; set; }
+
+    public void AddMoney(float amount)
+    {
+        Value += amount;
+    }
+
+    public void SubMoney(float amount)
+    {
+        Value -= amount;
+    }
+}
+
+public class Customer
+{
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public Wallet Wallet { get; set; }
+}
+
+public class Paperboy
+{
+    public void SellPaper(Customer customer)
+    {
+        var payment = 2.0f;
+        var wallet = customer.Wallet;
+        if (wallet.Value >= payment) wallet.SubMoney(payment);
+    }
+}
---
+++ Code/src/ObjectsandDataStructures/TheLawofDemeter/Good.cs
@@ -0,0 +1,61 @@
+﻿namespace TheLawofDemeter.Good;
+
+public class Wallet
+{
+    public Wallet(float initialAmount)
+    {
+        Value = initialAmount;
+    }
+
+    public float Value { get; private set; }
+
+    public void AddMoney(float amount)
+    {
+        Value += amount;
+    }
+
+    public void SubMoney(float amount)
+    {
+        Value -= amount;
+    }
+}
+
+public class Customer
+{
+    private readonly Wallet _wallet;
+
+    public Customer()
+    {
+        FirstName = "John";
+        LastName = "Doe";
+        _wallet = new Wallet(20.0f); // amount set to 20 for example
+    }
+
+    public string FirstName { get; }
+
+    public string LastName { get; }
+
+    public float PayAmount(float amountToPay)
+    {
+        if (_wallet.Value >= amountToPay)
+        {
+            _wallet.SubMoney(amountToPay);
+            return amountToPay;
+        }
+
+        return 0;
+    }
+}
+
+public class Paperboy
+{
+    public void SellPaper(Customer customer)
+    {
+        var payment = 2.0f;
+        var amountPaid = customer.PayAmount(payment);
+        if (amountPaid != payment)
+        {
+            // come back later
+        }
+    }
+}
