1c1ff969a251444482319bba0dbf88ca131300f8
Mansour Tarafdar <tarafdar.mansour@gmail.com>
2022-10-25 20:13:19 +0330

    feat (Chapter7): add example for Provide Context with Exceptions

---
+++ Code/src/ErrorHandling/ProvideContextwithExceptions/Bad.cs
@@ -0,0 +1,12 @@
+﻿namespace ProvideContextwithExceptions;
+
+public class Bad
+{
+    public double Divide(double numerator, double denominator)
+    {
+        if (denominator == 0)
+            throw new Exception("divide by zero");
+
+        return numerator / denominator;
+    }
+}
---
+++ Code/src/ErrorHandling/ProvideContextwithExceptions/Good.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+
+namespace ProvideContextwithExceptions;
+
+public class Good
+{
+    public double Divide(double numerator, double denominator)
+    {
+        if (denominator == 0) ThrowInformativeException(numerator, denominator);
+
+        return numerator / denominator;
+    }
+
+    private static void ThrowInformativeException(double numerator, double denominator)
+    {
+        throw new Exception(JsonConvert.SerializeObject(new
+        {
+            Message = "divide by zero",
+            Method = "Divide",
+            Class = "Good",
+            numerator,
+            denominator
+        }));
+    }
+}
